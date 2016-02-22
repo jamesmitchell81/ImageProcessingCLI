a624057bbabd8bee5ef8455210af807c36761861
jamesmitchell81 <james.mitchell81@live.co.uk>
2016-02-22 20:44:26 +0000

    retrieve contrast information of an image

--- ImageProcessingCLI/IP.h
+++ ImageProcessingCLI/IP.h
@@ -19,9 +19,14 @@
 - (NSBitmapImageRep*) maxFilterOfSize:(int)size onImage:(NSImage*)image;
 - (NSBitmapImageRep*) simpleAveragingFilterOfSize:(int)size onImage:(NSImage*)image;
 - (NSBitmapImageRep*) weightedAveragingFilterOfSize:(int)size onImage:(NSImage*)image;
-- (NSBitmapImageRep*) threshold:(NSImage*)image atValue:(int)value;
+
 - (NSImage*) reduceNoiseWithCIMedianFilterOnImage:(NSImage *)image;
 
+- (NSBitmapImageRep*) threshold:(NSImage*)image atValue:(int)value;
+- (int*) contrastHistogramOfImage:(NSImage*)image;
+
 - (NSBitmapImageRep*) imageDifferenceOf:(NSImage*)image1 and:(NSImage*)image2;
+
+- (NSBitmapImageRep*) imageNegativeOf:(NSImage*)image;
 
 @end
